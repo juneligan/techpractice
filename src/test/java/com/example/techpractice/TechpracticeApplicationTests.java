450281478e937677969e40a38cbfe9fd8e2a8ad3
june-at-256608879485 <ligan.june@gmail.com>
2023-07-07 23:41:40 +0800

    Initial Commit

---
+++ src/test/java/com/example/techpractice/TechpracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.techpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechpracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
